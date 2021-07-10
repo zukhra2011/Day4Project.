ed7a667ba8c005a5904ffd5e5749e3f05513295c
Zukhra Khaknazarova <zukhra2011@gmail.com>
2021-07-10 14:23:08 -0400

    Review 1 branch.

--- src/test/java/BranchingPractice.java
+++ src/test/java/BranchingPractice.java
@@ -5,6 +5,7 @@ public class BranchingPractice {
         //added.gitignore with .idea, target folder and iml file
         //made initial commit by keeping track of all files
         //shared project on github
+        //reviewing on new branch.
 
     }
 }
