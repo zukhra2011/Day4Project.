b4414f10dba674f64e7decfe1f8457d1a62efc2f
Zukhra Khaknazarova <zukhra2011@gmail.com>
2021-07-10 14:20:27 -0400

    Update.

--- src/test/java/BranchingPractice.java
+++ src/test/java/BranchingPractice.java
@@ -1,6 +1,10 @@
 public class BranchingPractice {
     public static void main(String[] args) {
-
+//created new project
+        //enables version control
+        //added.gitignore with .idea, target folder and iml file
+        //made initial commit by keeping track of all files
+        //shared project on github
 
     }
 }
